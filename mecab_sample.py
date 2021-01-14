5d79c9317f76fb20cd01289a1a97ff68ad92184c
niwa-niwa <niwayama1101@gmail.com>
2021-01-14 23:56:46 +0900

    add comments

--- mecab_sample.py
+++ mecab_sample.py
@@ -2,10 +2,12 @@ import MeCab
 
 
 tagger = MeCab.Tagger()
-tagger.parse('')
+tagger.parse('') # this is for avoiding a bug in .parseToNode()
 
+# parsing sentence 
 node = tagger.parseToNode('すもももももももものうち')
 
 while node:
+    # output .surface is 形態素 .feature is 品詞
     print(node.surface, node.feature)
     node = node.next
